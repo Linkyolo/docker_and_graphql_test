b3235a7bb48790a688ec6c010d3e8e69b0ac3df5
Linkyolo <alexjit95@gmail.com>
2025-07-19 12:14:24 +0200

    Refactor User & workouts resolver

--- src/entities/Workout.ts
+++ src/entities/Workout.ts
@@ -1,10 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Field, ObjectType, ID } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
 
+@ObjectType()
 @Entity()
 export class Workout {
+
+        @Field(() => ID)
         @PrimaryGeneratedColumn()
         id!: number;
 
+        @Field()
         @Column()
-        name!: string;
+        date?: Date;
+
+        @Field()
+        @Column()
+        type?: string;
+
+        @Field(() => User)
+        @ManyToOne(() => User, user => user.workouts, { onDelete: 'CASCADE' })
+        user: User = undefined!;
+
 }
--- src/graphql/resolvers/User.ts
+++ src/graphql/resolvers/User.ts
@@ -1,23 +1,25 @@
-
+import { Resolver, Query, Mutation, Arg } from 'type-graphql'
 import { User } from '../../entities/User';
+import { AppDataSource } from '../../data-source';
 
+@Resolver(() => User)
 export class UserResolver {
-        typeDefs = `
-    type User {
-      id: ID!
-      name: String!
-    }
+        private repo = AppDataSource.getRepository(User);
 
-    extend type Query {
-      users: [User!]!
-    }
-  `;
-
-        resolvers = {
-                Query: {
-                        users: async (_: any, __: any, { dataSource }) => {
-                                return await dataSource.getRepository(User).find();
-                        },
-                },
+        @Query(() => [User])
+        async users(): Promise<User[]> {
+                return this.repo.find({ relations: ['workouts'] })
         };
+
+
+        @Query(() => User, { nullable: true })
+        async user(@Arg('id') id: number): Promise<User | null> {
+                return this.repo.findOne({ where: { id }, relations: ['workouts'], });
+        }
+
+        @Mutation(() => User)
+        async createUser(@Arg('name') name: string): Promise<User> {
+                const user = this.repo.create({ name });
+                return this.repo.save(user)
+        }
 }
